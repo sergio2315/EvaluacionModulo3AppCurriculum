9676460b86c9a5af58dad30a9dbf37c7fff29704
Sergio Lillo <Sergiolillo2315@gmail.com>
2020-12-22 12:40:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/evaluacinmodulo3appcurriculum/ContactActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluacinmodulo3appcurriculum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
