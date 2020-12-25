54d3eb25f12bf6e94c418bd2d71c209d9ee7f53a
Sergio Lillo <Sergiolillo2315@gmail.com>
2020-12-24 23:18:51 -0300

    agregando fragmentos

--- app/src/main/java/com/example/evaluacinmodulo3appcurriculum/ContactActivity.java
+++ app/src/main/java/com/example/evaluacinmodulo3appcurriculum/ContactActivity.java
@@ -2,13 +2,43 @@ package com.example.evaluacinmodulo3appcurriculum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.evaluacinmodulo3appcurriculum.databinding.ActivityContactBinding;
 
 public class ContactActivity extends AppCompatActivity {
-
+private ActivityContactBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+
+        binding = ActivityContactBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.imageViewLinkedin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent linkedingIntent = new Intent(Intent.ACTION_VIEW);
+            linkedingIntent.setData(Uri.parse("https://www.linkedin.com/in/sergio-iv%C3%A1n-lillo-melo/"));
+            startActivity(linkedingIntent);
+            Toast.makeText(ContactActivity.this, "esto es una ventana emergente", Toast.LENGTH_SHORT)
+                    .show();
+    }
+});
+        binding.imageViewPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+        shareWithWsp("esto es un mensaje para enviar por whassapp");
+            }
+        });
+        }
+    public void shareWithWsp(String message){
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
     }
 }
--- app/src/main/java/com/example/evaluacinmodulo3appcurriculum/MainActivity.java
+++ app/src/main/java/com/example/evaluacinmodulo3appcurriculum/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.evaluacinmodulo3appcurriculum;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,9 +13,9 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +25,19 @@ public class MainActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
+        ImageView imageView = findViewById(R.id.imageViewMain);
+        Glide.with(this).load("https://unsplash.com/photos/m4pdE5s15xA/download?force=true&w=640")
+                .centerCrop()
+                .into(imageView);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent mIntent = new Intent(MainActivity.this,ContactActivity.class);
+                startActivity(mIntent);
             }
         });
+
     }
 
     @Override
